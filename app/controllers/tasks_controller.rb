852706b0b53f3f35d7dd4a06c8cd7eff9516a688
Zoheb Salam <zoheb_391@hotmail.com>
2016-12-13 22:23:04 +0000

    create task test passing

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,11 +9,16 @@ class TasksController < ApplicationController
     task.update_attributes(task_params)
     render json: task
   end
+
+  def create
+    task = Task.create(task_params)
+    render json: task
+  end
   
   private
 
   def task_params
-    params.require(:task).permit(:done)
+    params.require(:task).permit(:done, :title)
   end
 
 end
--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -29,4 +29,15 @@ RSpec.describe TasksController, type: :controller do
       expect(task.done).to eq(true)
     end
   end
+
+  describe "task#create" do
+    it "should allow tasks to be created" do
+      post :create, task: {title: "poop"}
+      expect(response).to have_http_status(:success)
+      response_value = ActiveSupport::JSON.decode(@response.body)
+      expect(response_value['title']).to eq("poop")
+      expect(Task.last.title).to eq("poop")
+    end
+  end
+
 end
